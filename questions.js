3d54c7fc66bd00228cf8421bff8cda059c8c367a
MasonSchnell <91274207+MasonSchnell@users.noreply.github.com>
2023-09-16 22:32:16 -0400

    Added the start of the highscore feature and a result prompt

---
+++ questions.js
@@ -0,0 +1,185 @@
+var questions = [
+    {
+        question: "What is full-stack development?",
+        answer: [
+            { text: "Front-end development", answer: false },
+            { text: "Back-end development", answer: false },
+            { text: "Front-end and Back-end development", answer: true },
+            { text: "None of the above", answer: false },
+        ],
+    },
+    {
+        question:
+            "Which programming language is commonly used for front-end development?",
+        answer: [
+            { text: "Python", answer: false },
+            { text: "Java", answer: false },
+            { text: "JavaScript", answer: true },
+            { text: "Ruby", answer: false },
+        ],
+    },
+    {
+        question: "What does HTML stand for?",
+        answer: [
+            { text: "Hyperlink Text Markup Language", answer: false },
+            { text: "Hypertext Transfer Markup Language", answer: true },
+            { text: "High-level Text Markup Language", answer: false },
+            { text: "Hyper Transferable Text Markup Language", answer: false },
+        ],
+    },
+    {
+        question:
+            "Which of the following is not a commonly used front-end development framework/library?",
+        answer: [
+            { text: "Angular", answer: false },
+            { text: "React", answer: false },
+            { text: "Django", answer: false },
+            { text: "Vue.js", answer: true },
+        ],
+    },
+    {
+        question:
+            "What is the purpose of a back-end framework like Express.js?",
+        answer: [
+            { text: "Handling front-end UI", answer: false },
+            { text: "Managing server-side logic and routing", answer: true },
+            { text: "Creating user interfaces", answer: false },
+            { text: "Storing database records", answer: false },
+        ],
+    },
+    {
+        question: "What is a RESTful API?",
+        answer: [
+            { text: "A type of database", answer: false },
+            {
+                text: "A web service that follows specific principles for interaction",
+                answer: true,
+            },
+            {
+                text: "A JavaScript library for building user interfaces",
+                answer: false,
+            },
+            { text: "A type of front-end framework", answer: false },
+        ],
+    },
+    {
+        question:
+            "In the context of web development, what does 'HTTP' stand for?",
+        answer: [
+            { text: "Hyperlink Transfer Protocol", answer: false },
+            { text: "Hypertext Transfer Protocol", answer: true },
+            { text: "Hyperlink Text Protocol", answer: false },
+            { text: "Hypertext Text Protocol", answer: false },
+        ],
+    },
+    {
+        question: "What is the purpose of version control systems like Git?",
+        answer: [
+            { text: "Managing databases", answer: false },
+            {
+                text: "Tracking changes in code and collaborating with others",
+                answer: true,
+            },
+            { text: "Writing HTML code", answer: false },
+            { text: "Testing web applications", answer: false },
+        ],
+    },
+    {
+        question:
+            "Which JavaScript framework/library is often used for building single-page applications (SPAs)?",
+        answer: [
+            { text: "Angular", answer: true },
+            { text: "Django", answer: false },
+            { text: "Laravel", answer: false },
+            { text: "Ruby on Rails", answer: false },
+        ],
+    },
+    {
+        question: "What does MVC stand for in the context of web development?",
+        answer: [
+            { text: "Model, View, Controller", answer: true },
+            { text: "Manage, View, Control", answer: false },
+            { text: "Main, View, Component", answer: false },
+            { text: "Master, Visualize, Connect", answer: false },
+        ],
+    },
+    {
+        question:
+            "Which front-end framework/library allows for the creation of interactive and reusable UI components?",
+        answer: [
+            { text: "React", answer: true },
+            { text: "Vue.js", answer: false },
+            { text: "Ember.js", answer: false },
+            { text: "jQuery", answer: false },
+        ],
+    },
+    {
+        question:
+            "What is the purpose of a 'package.json' file in a Node.js project?",
+        answer: [
+            { text: "To store user data", answer: false },
+            {
+                text: "To define project dependencies and configuration",
+                answer: true,
+            },
+            { text: "To create RESTful APIs", answer: false },
+            { text: "To manage front-end assets", answer: false },
+        ],
+    },
+    {
+        question: "What does SQL stand for?",
+        answer: [
+            { text: "Simple Query Language", answer: false },
+            { text: "Structured Query Language", answer: true },
+            { text: "Standard Query Language", answer: false },
+            { text: "Server Query Language", answer: false },
+        ],
+    },
+    {
+        question:
+            "What is the primary function of a web server in the context of full-stack development?",
+        answer: [
+            { text: "Displaying web pages", answer: false },
+            { text: "Managing client-side scripting", answer: false },
+            {
+                text: "Handling incoming HTTP requests and serving web content",
+                answer: true,
+            },
+            { text: "Storing user login credentials", answer: false },
+        ],
+    },
+    {
+        question: "What is the purpose of the 'document object' in JavaScript?",
+        answer: [
+            { text: "Accessing the user's webcam", answer: false },
+            {
+                text: "Manipulating the HTML and CSS of a web page",
+                answer: true,
+            },
+            { text: "Sending HTTP requests", answer: false },
+            { text: "Managing server-side logic", answer: false },
+        ],
+    },
+    {
+        question: "What is a 'cookie' in web development?",
+        answer: [
+            {
+                text: "A small text file stored on the client's device",
+                answer: true,
+            },
+            { text: "A type of database", answer: false },
+            { text: "A front-end framework", answer: false },
+            { text: "A form of authentication", answer: false },
+        ],
+    },
+    {
+        question:
+            "Which of the following is not a commonly used front-end development framework/library?",
+        answer: [
+            { text: "Angular", answer: false },
+            { text: "React", answer: false },
+            { text: "Django", answer: true },
+            { text: "Vue.js", answer: false },
+        ],
+    },
+];
